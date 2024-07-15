8e7acf284a52f115ef50072017821d118c4ffb3b
PETERIPAII.C <107237414+Peteripai@users.noreply.github.com>
2024-07-15 19:15:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed('#typed-output', {
+      strings: ["Web Developer", "Web Designer", "Technology Enthusiast"],
+      typeSpeed: 60,
+      backSpeed: 30,
+      loop: true
+    });
+  });
+  
+  function toggleMenu() {
+    const menu = document.querySelector('.menu-links');
+    menu.classList.toggle('open');
+  }
+  
