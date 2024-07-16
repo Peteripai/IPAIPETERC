5b3a2d225af1f43b722bbc3c424e666a9e5dbc55
PETERIPAII.C <107237414+Peteripai@users.noreply.github.com>
2024-07-16 12:07:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function toggleMenu() {
 
   document.addEventListener('DOMContentLoaded', function() {
     var typed = new Typed('#typed-output', {
-      strings: ["I AM a Web Developer", "Web Designer", "Technology Enthusiast"],
+      strings: ["I AM a Web Developer,Web designer and a Technology Enthusiast",],
       typeSpeed: 60,
       backSpeed: 30,
       loop: true
